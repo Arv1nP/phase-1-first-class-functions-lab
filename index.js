592325fe298ad7075745ca17c82df5ff23894b6f
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-08-22 02:51:34 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(x) {
+    let result = [...x]
+    return result.slice(0,2);
+}
+
+function returnLastTwoDrivers(y){
+    let result = [...y]
+    return result.slice(2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier() {
+    return function (fare) {
+      return fare * fare;
+    };
+  }
+  function fareDoubler(fare) {
+    return fare * 2;
+  }
+  function fareTripler(fare) {
+    return fare * 3;
+  }
+  function selectDifferentDrivers(drivers,b){
+    return b(drivers);
+  }
